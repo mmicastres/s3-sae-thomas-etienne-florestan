c43de969170cb4ffe987caba8348953b739a9972
SautDeBiquette <etienne.gonella@gmail.com>
2023-12-12 15:27:02 +0100

    UpdateCodeCursor

--- cursors.js
+++ cursors.js
@@ -1,7 +1,6 @@
 let cursor = Math.floor(Math.random()*13);
 console.log(cursor);
 
-let corps = document.querySelector("body");
-corps.style.cursor = `url(cursors/Click${cursor}.svg)`;
+const urlCursor = `url('cursors/Click${cursor}.svg')`;
 
-document.body.style.cursor = url("cursors/Click${cursor}.svg");
+document.querySelector("html").style.cursor = urlCursor+", default";
