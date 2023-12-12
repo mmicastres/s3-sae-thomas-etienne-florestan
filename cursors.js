473ab453a3f647829b3ae58165c61e0be3b34a7c
SautDeBiquette <etienne.gonella@gmail.com>
2023-12-12 15:32:21 +0100

    BonCodeJsCursors

--- cursors.js
+++ cursors.js
@@ -1,13 +1,6 @@
 let cursor = Math.floor(Math.random()*13);
 console.log(cursor);
 
-<<<<<<<<< Temporary merge branch 1
 const urlCursor = `url('cursors/Click${cursor}.svg')`;
 
 document.querySelector("html").style.cursor = urlCursor+", default";
-=========
-// let corps = document.querySelector("body");
-// corps.style.cursor = `url(cursors/Click${cursor}.svg)`;
-
-// document.body.style.cursor = url("cursors/Click${cursor}.svg");
->>>>>>>>> Temporary merge branch 2
