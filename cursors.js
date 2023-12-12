05505596723bcea59f1153e8ca46c298fdd1c8e6
SautDeBiquette <etienne.gonella@gmail.com>
2023-12-12 14:44:23 +0100

    essai Cursor

--- cursors.js
+++ cursors.js
@@ -4,3 +4,4 @@ console.log(cursor);
 let corps = document.querySelector("body");
 corps.style.cursor = `url(cursors/Click${cursor}.svg)`;
 
+document.body.style.cursor = url("cursors/Click${cursor}.svg");
