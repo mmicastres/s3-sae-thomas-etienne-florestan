ed573e4712f83c298a5527f6ec03c69fa6c639f9
Thomas ZABALO <thomas.zabalo@gmail.com>
2023-12-12 15:28:38 +0100

    ouiiiii

--- cursors.js
+++ cursors.js
@@ -1,7 +1,7 @@
-let cursor = Math.floor(Math.random()*13);
-console.log(cursor);
+// let cursor = Math.floor(Math.random()*13);
+// console.log(cursor);
 
-let corps = document.querySelector("body");
-corps.style.cursor = `url(cursors/Click${cursor}.svg)`;
+// let corps = document.querySelector("body");
+// corps.style.cursor = `url(cursors/Click${cursor}.svg)`;
 
-document.body.style.cursor = url("cursors/Click${cursor}.svg");
+// document.body.style.cursor = url("cursors/Click${cursor}.svg");
