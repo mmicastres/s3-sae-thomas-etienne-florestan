5d413dcf5e87034f8b14260a722171cae40180b7
SautDeBiquette <etienne.gonella@gmail.com>
2024-01-10 11:32:06 +0100

    essai avec mouseover

--- cursors.js
+++ cursors.js
@@ -5,9 +5,10 @@ const urlDefault = `url('cursors/Click${cursor}.svg')`;
 const urlGrab = `url('cursors/Grab${cursor}.svg')`;
 
 document.querySelector("html").style.cursor = urlDefault + ", default";
-document.querySelector("html").style.cursor = urlGrab + ", pointer";
+// document.querySelector("html").style.cursor = urlGrab + ", pointer";
 
 
+document.querySelector("html").addEventListener('mouseover', () => { document.querySelector("html").style.cursor = urlGrab + ", pointer"; })
 
 
 
