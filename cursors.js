83080d93a6c9f7e83ff24bd4c916216d9af19216
SautDeBiquette <etienne.gonella@gmail.com>
2024-01-10 11:26:09 +0100

    finalisation du code curseur

--- cursors.js
+++ cursors.js
@@ -5,8 +5,9 @@ const urlDefault = `url('cursors/Click${cursor}.svg')`;
 const urlGrab = `url('cursors/Grab${cursor}.svg')`;
 
 document.querySelector("html").style.cursor = urlDefault + ", default";
+document.querySelector("html").style.cursor = urlDefault + ", grab";
 
-document.querySelector("html").addEventListener('mouseover', () => { document.querySelector("html").style.cursor = urlDefault + ", default"; })
+
 
 
 
