0c7541b7f66fdcfcbffd364e1a6511151ed78626
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-02-01 11:38:08 +0100

    change

--- script.js
+++ script.js
@@ -145,21 +145,21 @@ fetch(url, fetchOptions)
     document.querySelector(".carousel-item").classList.add("active");
 
 
-    const mixedChart = new Chart(ctx, {
-      data: {
-          datasets: [{
-              type: 'bar',
-              label: 'Bar Dataset',
-              data: [10, 20, 30, 40]
-          }, {
-              type: 'line',
-              label: 'Line Dataset',
-              data: [50, 50, 50, 50],
-          }],
-          labels: ['January', 'February', 'March', 'April']
-      },
-      options: options
-  });
+  //   const mixedChart = new Chart(ctx, {
+  //     data: {
+  //         datasets: [{
+  //             type: 'bar',
+  //             label: 'Bar Dataset',
+  //             data: [10, 20, 30, 40]
+  //         }, {
+  //             type: 'line',
+  //             label: 'Line Dataset',
+  //             data: [50, 50, 50, 50],
+  //         }],
+  //         labels: ['January', 'February', 'March', 'April']
+  //     },
+  //     options: options
+  // });
   
 
 
