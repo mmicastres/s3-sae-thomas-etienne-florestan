b6ca7b5128779fe143e3a886e1b720bc0ba7daad
Florestan Mellé-Cappellin <150163972+FlorestanMC@users.noreply.github.com>
2024-02-02 22:57:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -123,11 +123,7 @@ fetch(url, fetchOptions)
                         </div>
                         
                         <div class="text text-right py-2 pl-2">
-<<<<<<< Updated upstream
                           Show more
-=======
-                          See more
->>>>>>> Stashed changes
                         </div>
                       </button>
                     </div>
